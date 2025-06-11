9830def0afc318527b88cdab1d45bd0afe6c18ac
DanielCr <u20221g120@upc.edu.pe>
2025-06-11 11:29:02 -0500

    feat: add interface for commands and query services

---
+++ SmartEdu.Demy.Platform.API/Scheduling/Domain/Services/ICourseCommandService.cs
@@ -0,0 +1,13 @@
+using SmartEdu.Demy.Platform.API.Scheduling.Domain.Model.Commands;
+using SmartEdu.Demy.Platform.API.Scheduling.Domain.Model.Entities;
+
+namespace SmartEdu.Demy.Platform.API.Scheduling.Domain.Services;
+
+public interface ICourseCommandService
+{
+    Task<Course?> Handle(CreateCourseCommand command);
+
+    Task<Course?> Handle(UpdateCourseCommand command);
+
+    Task<bool> Handle(DeleteCourseCommand command);
+}
