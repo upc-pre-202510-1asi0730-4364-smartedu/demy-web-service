8c08cd37de9fc6ba30e1c0da9895d95f5c716632
Kyrubi <u20221c486@upc.edu.pe>
2025-06-11 16:14:31 -0500

    feat(enrollment): add StudentRepository implementation

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Infrastructure/Persistence/EFC/Repositories/StudentRepository.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Entities;
+using SmartEdu.Demy.Platform.API.Enrollment.Domain.Repositories;
+using SmartEdu.Demy.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using SmartEdu.Demy.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace SmartEdu.Demy.Platform.API.Enrollment.Infrastructure.Persistence.EFC.Repositories;
+
+public class StudentRepository(AppDbContext context) 
+    : BaseRepository<Student>(context), IStudentRepository
+{
+    // aca se podra añadir metodos especificos para student en el futuro.
+    // solo por ahora hereda completamente de BaseRepository.
+}
