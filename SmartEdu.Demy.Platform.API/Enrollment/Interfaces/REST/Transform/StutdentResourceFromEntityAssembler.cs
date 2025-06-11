6398091a0ca1ae71bfe6e235db24dadc1ce29c57
Kyrubi <u20221c486@upc.edu.pe>
2025-06-11 16:16:03 -0500

    feat(enrollment): add StudentResourceFromEntityAssembler in enrollment context

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Interfaces/REST/Transform/StutdentResourceFromEntityAssembler.cs
@@ -0,0 +1,23 @@
+﻿using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Entities;
+using SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Resources;
+
+namespace SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Transform;
+
+public static class StudentResourceFromEntityAssembler
+{
+    public static StudentResource ToResourceFromEntity(Student entity)
+    {
+        return new StudentResource(
+            entity.Id,
+            entity.FirstName,
+            entity.LastName,
+            entity.Dni,
+            entity.Sex.ToString(),
+            entity.BirthDate,
+            entity.Address,
+            entity.PhoneNumber,
+            entity.CreatedDate,
+            entity.UpdatedDate
+        );
+    }
+}
