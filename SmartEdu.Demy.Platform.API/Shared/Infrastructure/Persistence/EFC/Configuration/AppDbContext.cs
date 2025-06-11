9c6c0d0450707cd1aa5593c01537070487a735bd
Kyrubi <u20221c486@upc.edu.pe>
2025-06-11 15:58:48 -0500

    feat(shared): update AppDbContext.cs to EF Core mapping with enrollmnet context

--- SmartEdu.Demy.Platform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
+++ SmartEdu.Demy.Platform.API/Shared/Infrastructure/Persistence/EFC/Configuration/AppDbContext.cs
@@ -2,6 +2,8 @@ using EntityFrameworkCore.CreatedUpdatedDate.Extensions;
 using Microsoft.EntityFrameworkCore;
 using SmartEdu.Demy.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration.Extensions;
 
+using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Entities;
+
 namespace SmartEdu.Demy.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 
 /// <summary>
@@ -21,7 +23,43 @@ public class AppDbContext(DbContextOptions options) : DbContext(options)
         base.OnModelCreating(builder);
         
         // Billing Context
+        
+        
+// Enrollment Context
 
+        builder.Entity<Student>().HasKey(s => s.Id);
+
+        builder.Entity<Student>().Property(s => s.Id)
+            .IsRequired()
+            .ValueGeneratedOnAdd();
+
+        builder.Entity<Student>().Property(s => s.FirstName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Entity<Student>().Property(s => s.LastName)
+            .IsRequired()
+            .HasMaxLength(100);
+
+        builder.Entity<Student>().Property(s => s.Dni)
+            .IsRequired()
+            .HasMaxLength(20);
+
+        builder.Entity<Student>().Property(s => s.Sex)
+            .IsRequired()
+            .HasConversion<string>()  // Si ESex es enum, lo guardamos como string
+            .HasMaxLength(10);
+
+        builder.Entity<Student>().Property(s => s.BirthDate)
+            .IsRequired(false);
+
+        builder.Entity<Student>().Property(s => s.Address)
+            .IsRequired()
+            .HasMaxLength(255);
+
+        builder.Entity<Student>().Property(s => s.PhoneNumber)
+            .HasMaxLength(9);
+        
         // Aquí irá lo demás
         
         builder.UseSnakeCaseNamingConvention();
