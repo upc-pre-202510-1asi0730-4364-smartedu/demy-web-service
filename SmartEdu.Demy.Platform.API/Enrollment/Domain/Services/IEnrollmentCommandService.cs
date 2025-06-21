4d0b5927eab6063a468b2f81094faf2bed972b00
Kyrubi <u20221c486@upc.edu.pe>
2025-06-21 14:35:37 -0500

    feat(enrollment): add IEnrollmentCommandService interface

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Domain/Services/IEnrollmentCommandService.cs
@@ -0,0 +1,10 @@
+﻿using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Commands;
+
+namespace SmartEdu.Demy.Platform.API.Enrollment.Domain.Services;
+
+public interface IEnrollmentCommandService
+{
+    Task<Model.Aggregates.Enrollment?> Handle(CreateEnrollmentCommand command);
+    Task<Model.Aggregates.Enrollment?> Handle(UpdateEnrollmentCommand command);
+    Task<bool> Handle(DeleteEnrollmentCommand command);
+}
