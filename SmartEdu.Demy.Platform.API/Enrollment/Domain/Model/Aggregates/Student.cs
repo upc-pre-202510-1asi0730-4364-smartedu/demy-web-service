f369ea3d4c508e297b88d91865683bcebdb18fa4
Kyrubi <u20221c486@upc.edu.pe>
2025-06-21 14:40:08 -0500

    feat(enrollment): add Student in aggregate root

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Domain/Model/Aggregates/Student.cs
@@ -0,0 +1,64 @@
+﻿using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Commands;
+using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.ValueObjects;
+
+namespace SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Aggregates;
+
+public partial class Student
+{
+    public int Id { get; set; }
+    public PersonName Name{ get; private set; }
+    public Dni Dni { get; private set; }    
+    public ESex Sex { get; private set; }
+    public DateTime BirthDate { get; private set; }
+    public string Address { get; private set; }
+    public PhoneNumber PhoneNumber { get; private set; }
+    
+    private Student() { }   
+
+    
+    public Student(string firstName, string lastName, string dni, ESex sex, DateTime birthDate, string address, string phoneNumber)
+    {
+        Name = new PersonName(firstName, lastName);
+        Dni = new Dni(dni);
+        Sex = sex;
+        BirthDate = birthDate;
+        Address = address;
+        PhoneNumber = new PhoneNumber(phoneNumber);
+    }
+
+    public Student(CreateStudentCommand command)
+        : this(
+            command.FirstName,
+            command.LastName,
+            command.Dni,
+            ParseSex(command.Sex),
+            command.BirthDate,
+            command.Address,
+            command.PhoneNumber)
+    {
+    }
+    private static ESex ParseSex(string sexValue)
+    {
+        if (Enum.TryParse<ESex>(sexValue, ignoreCase: true, out var sex))
+            return sex;
+        throw new ArgumentException($"Valor de sexo inválido: '{sexValue}'", nameof(sexValue));
+    }
+    
+    public Student UpdateInformation(
+        string firstName,
+        string lastName,
+        string dni,
+        ESex sex,
+        DateTime birthDate,
+        string address,
+        string phoneNumber)
+    {
+        Name = new PersonName(firstName, lastName);
+        Dni = new Dni(dni);
+        Sex = sex;
+        BirthDate = birthDate;
+        Address = address;
+        PhoneNumber = new PhoneNumber(phoneNumber);
+        return this;
+    }
+}
