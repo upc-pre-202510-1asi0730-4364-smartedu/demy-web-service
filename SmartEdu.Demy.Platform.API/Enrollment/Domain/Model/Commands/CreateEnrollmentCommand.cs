237925ca980c4315cdbf5ead46e9fbae0a0a5971
Kyrubi <u20221c486@upc.edu.pe>
2025-06-21 14:27:31 -0500

    feat(enrollment): add CreateEnrollmentCommand in domain model

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Domain/Model/Commands/CreateEnrollmentCommand.cs
@@ -0,0 +1,7 @@
+﻿namespace SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Commands;
+
+    /// <summary>
+    /// Command para crear un nuevo Enrollment
+    /// </summary>
+public record CreateEnrollmentCommand(int StudentId, int PeriodId, decimal Amount, string Currency, string EnrollmentStatus, string PaymentStatus);
+
