80c4f1c1bfc293b7f291516297b519ab52e39891
DanielCr <u20221g120@upc.edu.pe>
2025-06-20 20:10:35 -0500

    feat(scheduling): add resources of weekly schedule

---
+++ SmartEdu.Demy.Platform.API/Scheduling/Interfaces/REST/Resources/AddScheduleToWeeklyResource.cs
@@ -0,0 +1,8 @@
+namespace SmartEdu.Demy.Platform.API.Scheduling.Interfaces.REST.Resources;
+
+public record AddScheduleToWeeklyResource(
+    string DayOfWeek,
+    string StartTime,
+    string EndTime,
+    int CourseId,
+    int ClassroomId);
