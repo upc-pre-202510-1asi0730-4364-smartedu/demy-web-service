2cad3809fbf0d33724109763c72a9687896ab783
Kyrubi <u20221c486@upc.edu.pe>
2025-06-21 14:42:23 -0500

    feat(enrollment): add UpdateEnrollmentCommandFromResourceAssembler

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Interfaces/REST/Transform/UpdateEnrollmentCommandFromResourceAssembler.cs
@@ -0,0 +1,19 @@
+﻿using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Commands;
+using SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Resources;
+
+namespace SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Transform
+{
+    public static class UpdateEnrollmentCommandFromResourceAssembler
+    {
+        public static UpdateEnrollmentCommand ToCommandFromResource(int enrollmentId, UpdateEnrollmentResource resource)
+        {
+            return new UpdateEnrollmentCommand(
+                enrollmentId,
+                resource.Amount,
+                resource.Currency,
+                resource.EnrollmentStatus,
+                resource.PaymentStatus
+            );
+        }
+    }
+}
