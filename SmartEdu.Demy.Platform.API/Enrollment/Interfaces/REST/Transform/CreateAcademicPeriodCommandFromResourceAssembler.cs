654ea9ec96b8b9ad2ad231a070b4db0dd9325837
Kyrubi <u20221c486@upc.edu.pe>
2025-06-21 14:27:09 -0500

    feat(enrollment): add CreateAcademicPeriodCommandFromResourceAssembler

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Interfaces/REST/Transform/CreateAcademicPeriodCommandFromResourceAssembler.cs
@@ -0,0 +1,16 @@
+﻿using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Commands;
+using SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Resources;
+
+namespace SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Transform;
+
+public static class CreateAcademicPeriodCommandFromResourceAssembler
+{
+    public static CreateAcademicPeriodCommand ToCommandFromResource(CreateAcademicPeriodResource resource)
+    {
+        return new CreateAcademicPeriodCommand(
+        resource.PeriodName, 
+        resource.StartDate, 
+        resource.EndDate, 
+        resource.IsActive);
+    }
+}
