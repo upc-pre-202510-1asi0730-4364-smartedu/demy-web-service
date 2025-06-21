c7d6da9e336fb98aaf7b2fcefb1e7e498e9f21f1
Kyrubi <u20221c486@upc.edu.pe>
2025-06-21 14:42:57 -0500

    feat(enrollment): add UpdateStudentResource

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Interfaces/REST/Resources/UpdateStudentResource.cs
@@ -0,0 +1,11 @@
+﻿namespace SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Resources;
+
+public record UpdateStudentResource(
+    string FirstName,
+    string LastName,
+    string Dni,
+    string Sex,
+    DateTime BirthDate,
+    string Address,
+    string PhoneNumber
+);
