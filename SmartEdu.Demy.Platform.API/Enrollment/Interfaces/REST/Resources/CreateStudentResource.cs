ebab7872d4d806d303d3bf1a37d4978c9bb550a8
Kyrubi <u20221c486@upc.edu.pe>
2025-06-21 14:29:49 -0500

    feat(enrollment): add CreateStudentResource in REST resources

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Interfaces/REST/Resources/CreateStudentResource.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Resources;
+
+public record CreateStudentResource(
+    string FirstName,
+    string LastName,
+    string Dni,
+    string Sex,
+    DateTime BirthDate,
+    string Address,
+    string PhoneNumber);
