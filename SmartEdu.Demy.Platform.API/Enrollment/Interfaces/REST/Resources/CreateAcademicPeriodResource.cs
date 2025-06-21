7d27c623910cab8b99b1817c897cc17e05ef2d4f
Kyrubi <u20221c486@upc.edu.pe>
2025-06-21 14:27:19 -0500

    feat(enrollment): add CreateAcademicPeriodResource model

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Interfaces/REST/Resources/CreateAcademicPeriodResource.cs
@@ -0,0 +1,3 @@
+﻿namespace SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Resources;
+
+public record CreateAcademicPeriodResource(string PeriodName, DateTime StartDate, DateTime EndDate, bool IsActive);
