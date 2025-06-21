203b8e6a2b0fc1274ac08956cca4a5489a5a74b2
Kyrubi <u20221c486@upc.edu.pe>
2025-06-21 14:28:14 -0500

    feat(enrollment): add CreateStudentCommandFromResourceAssembler

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Interfaces/REST/Transform/CreateStudentCommandFromResourceAssembler.cs
@@ -0,0 +1,20 @@
+﻿using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Commands;
+using SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Resources;
+
+namespace SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Transform;
+
+public static class CreateStudentCommandFromResourceAssembler
+{
+    public static CreateStudentCommand ToCommandFromResource(CreateStudentResource resource)
+    {
+        return new CreateStudentCommand(
+            resource.FirstName,
+            resource.LastName,
+            resource.Dni,
+            resource.Sex,
+            resource.BirthDate,
+            resource.Address,
+            resource.PhoneNumber
+        );
+    }
+}
