d5ea6f86d02b0927d790707e62d807e60098bdcd
DanielCr <u20221g120@upc.edu.pe>
2025-06-20 20:07:52 -0500

    feat(scheduling): add resources from entity assembler of course

--- SmartEdu.Demy.Platform.API/Scheduling/Interfaces/REST/Transform/CourseResourceFromEntityAssembler.cs
+++ SmartEdu.Demy.Platform.API/Scheduling/Interfaces/REST/Transform/CourseResourceFromEntityAssembler.cs
@@ -1,4 +1,4 @@
-using SmartEdu.Demy.Platform.API.Scheduling.Domain.Model.Entities;
+using SmartEdu.Demy.Platform.API.Scheduling.Domain.Model.Aggregates;
 using SmartEdu.Demy.Platform.API.Scheduling.Interfaces.REST.Resources;
 
 namespace SmartEdu.Demy.Platform.API.Scheduling.Interfaces.REST.Transform;
