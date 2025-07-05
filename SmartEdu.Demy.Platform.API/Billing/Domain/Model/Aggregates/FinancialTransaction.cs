1f8a12b7438d164059d10fcf4efa505a0ef8453d
Salim Ramirez <salimramirezm@gmail.com>
2025-07-05 18:01:57 -0500

    feat(billing): add FinancialTransaction aggregate in billing context

---
+++ SmartEdu.Demy.Platform.API/Billing/Domain/Model/Aggregates/FinancialTransaction.cs
@@ -0,0 +1,28 @@
+using SmartEdu.Demy.Platform.API.Billing.Domain.Model.Entities;
+using SmartEdu.Demy.Platform.API.Billing.Domain.Model.ValueObjects;
+
+namespace SmartEdu.Demy.Platform.API.Billing.Domain.Model.Aggregates;
+
+public partial class FinancialTransaction
+{
+    public int Id { get; }
+
+    public ETransactionType Type { get; private set; }
+
+    public ETransactionCategory Category { get; private set; }
+
+    public string Concept { get; private set; }
+
+    public DateTime Date { get; private set; }
+
+    public Payment Payment { get; private set; }
+
+    public FinancialTransaction(string type, string category, string concept, DateTime date, Payment payment)
+    {
+        Type = Enum.Parse<ETransactionType>(type, true);
+        Category = Enum.Parse<ETransactionCategory>(category, true);
+        Concept = concept;
+        Date = date;
+        Payment = payment;
+    }
+}
