cff72f25bb2176c431421faed2685474ebe915e6
Salim Ramirez <salimramirezm@gmail.com>
2025-07-05 16:19:23 -0500

    feat(billing): add handle method for CreateInvoiceCommand

---
+++ SmartEdu.Demy.Platform.API/Billing/Domain/Services/IInvoiceCommandService.cs
@@ -0,0 +1,8 @@
+using SmartEdu.Demy.Platform.API.Billing.Domain.Model.Commands;
+
+namespace SmartEdu.Demy.Platform.API.Billing.Domain.Services;
+
+public interface IInvoiceCommandService
+{
+    Task<int?> Handle(CreateInvoiceCommand command);
+}
