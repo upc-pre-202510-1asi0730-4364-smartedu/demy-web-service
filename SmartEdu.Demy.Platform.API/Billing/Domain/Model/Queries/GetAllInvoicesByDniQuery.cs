b9c15f8e8d80cb246fb503a461d2afa4cce6d76a
Salim Ramirez <salimramirezm@gmail.com>
2025-07-05 16:17:20 -0500

    feat(billing): add get all invoices by dni query

---
+++ SmartEdu.Demy.Platform.API/Billing/Domain/Model/Queries/GetAllInvoicesByDniQuery.cs
@@ -0,0 +1,5 @@
+using SmartEdu.Demy.Platform.API.Shared.Domain.Model.ValueObjects;
+
+namespace SmartEdu.Demy.Platform.API.Billing.Domain.Model.Queries;
+
+public record GetAllInvoicesByDniQuery(Dni dni);
