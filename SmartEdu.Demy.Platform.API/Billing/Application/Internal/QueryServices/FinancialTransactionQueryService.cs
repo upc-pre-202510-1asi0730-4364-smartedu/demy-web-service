6eb7b110cf2888f8444e6ecc0d7094a770a9b28a
Salim Ramirez <salimramirezm@gmail.com>
2025-07-05 18:18:23 -0500

    feat(billing): add FinancialTransactionQueryService implementation

---
+++ SmartEdu.Demy.Platform.API/Billing/Application/Internal/QueryServices/FinancialTransactionQueryService.cs
@@ -0,0 +1,14 @@
+using SmartEdu.Demy.Platform.API.Billing.Domain.Model.Aggregates;
+using SmartEdu.Demy.Platform.API.Billing.Domain.Model.Queries;
+using SmartEdu.Demy.Platform.API.Billing.Domain.Repositories;
+using SmartEdu.Demy.Platform.API.Billing.Domain.Services;
+
+namespace SmartEdu.Demy.Platform.API.Billing.Application.Internal.QueryServices;
+
+public class FinancialTransactionQueryService(IFinancialTransactionRepository financialTransactionRepository) : IFinancialTransactionQueryService
+{
+    public async Task<IEnumerable<FinancialTransaction>> Handle(GetAllFinancialTransactionsQuery query)
+    {
+        return await financialTransactionRepository.ListAsync();
+    }
+}
