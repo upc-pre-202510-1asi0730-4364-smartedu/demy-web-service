eb89a5cc0aeab415401c136af5c0493a820c6d4e
Salim Ramirez <salimramirezm@gmail.com>
2025-07-05 16:15:14 -0500

    feat(billing): add create invoice command for invoice aggregate

---
+++ SmartEdu.Demy.Platform.API/Billing/Domain/Model/Commands/CreateInvoiceCommand.cs
@@ -0,0 +1,8 @@
+namespace SmartEdu.Demy.Platform.API.Billing.Domain.Model.Commands;
+
+public record CreateInvoiceCommand(
+    string dni,
+    decimal amount,
+    string currency,
+    DateOnly date
+);
