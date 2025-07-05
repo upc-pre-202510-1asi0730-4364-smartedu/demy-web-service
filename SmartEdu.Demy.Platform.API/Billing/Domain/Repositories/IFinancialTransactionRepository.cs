1beecf2154a7b98e64a27514f610ee94e1533c38
Salim Ramirez <salimramirezm@gmail.com>
2025-07-05 18:08:33 -0500

    feat(billing): add FinancialTransactionRepository interface

---
+++ SmartEdu.Demy.Platform.API/Billing/Domain/Repositories/IFinancialTransactionRepository.cs
@@ -0,0 +1,9 @@
+using SmartEdu.Demy.Platform.API.Billing.Domain.Model.Aggregates;
+using SmartEdu.Demy.Platform.API.Shared.Domain.Repositories;
+
+namespace SmartEdu.Demy.Platform.API.Billing.Domain.Repositories;
+
+public interface IFinancialTransactionRepository : IBaseRepository<FinancialTransaction>
+{
+
+}
