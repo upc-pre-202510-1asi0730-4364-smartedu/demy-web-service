7f5052e4f39691dca0b53711fac4f9b505174f90
diegovilcasaboya <diegovilcatut@gmail.com>
2025-07-06 18:24:35 -0500

    feat: add EnrollmentsContextFacade ACL

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Application/ACL/EnrollmentsContextFacade.cs
@@ -0,0 +1,17 @@
+﻿using SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.Queries;
+using SmartEdu.Demy.Platform.API.Enrollment.Domain.Services;
+using SmartEdu.Demy.Platform.API.Enrollment.Interfaces.ACL;
+
+
+namespace SmartEdu.Demy.Platform.API.Enrollment.Application.ACL;
+
+public class EnrollmentsContextFacade(IStudentQueryService studentQueryService) : IEnrollmentsContextFacade
+{
+    /// <inheritdoc />
+    public async Task<string> FetchStudentFullNameByDni(string dni)
+    {
+        var getStudentByDniQuery = new GetStudentByDniQuery(dni);
+        var student = await studentQueryService.Handle(getStudentByDniQuery);
+        return student?.Name.FullName ?? "";
+    }
+}
