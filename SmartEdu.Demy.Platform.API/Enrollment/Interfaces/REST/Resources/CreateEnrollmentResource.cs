becf289a8f501d3db88867bd3fb1d73b8fb98c4f
Kyrubi <u20221c486@upc.edu.pe>
2025-07-06 11:00:08 -0500

    feat(enrollment): add weeklyScheduleName parameter to CreateEnrollmentResource

--- SmartEdu.Demy.Platform.API/Enrollment/Interfaces/REST/Resources/CreateEnrollmentResource.cs
+++ SmartEdu.Demy.Platform.API/Enrollment/Interfaces/REST/Resources/CreateEnrollmentResource.cs
@@ -1,3 +1,3 @@
 ﻿namespace SmartEdu.Demy.Platform.API.Enrollment.Interfaces.REST.Resources;
 
-public record CreateEnrollmentResource(int StudentId, int AcademicPeriodId, decimal Amount, string Currency, string EnrollmentStatus, string PaymentStatus);
+public record CreateEnrollmentResource(int StudentId, int AcademicPeriodId, string WeeklyScheduleName, decimal Amount, string Currency, string EnrollmentStatus, string PaymentStatus);
