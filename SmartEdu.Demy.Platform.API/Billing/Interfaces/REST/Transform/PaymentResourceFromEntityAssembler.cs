f98ea3d757ef8d49de80d40277cbcf53b2fe5b66
Salim Ramirez <salimramirezm@gmail.com>
2025-07-06 16:18:40 -0500

    feat(billing): add validations in PaymentResourceFromEntityAssembler.cs

--- SmartEdu.Demy.Platform.API/Billing/Interfaces/REST/Transform/PaymentResourceFromEntityAssembler.cs
+++ SmartEdu.Demy.Platform.API/Billing/Interfaces/REST/Transform/PaymentResourceFromEntityAssembler.cs
@@ -7,6 +7,15 @@ public static class PaymentResourceFromEntityAssembler
 {
     public static PaymentResource ToResourceFromEntity(Payment entity)
     {
+        if (entity is null)
+            throw new ArgumentNullException(nameof(entity), "Payment entity is null");
+
+        if (entity.MonetaryAmount is null)
+            throw new NullReferenceException("Payment.MonetaryAmount is null");
+
+        if (entity.MonetaryAmount.Currency is null)
+            throw new NullReferenceException("Payment.MonetaryAmount.Currency is null");
+
         return new PaymentResource(
             entity.Id,
             entity.MonetaryAmount.Amount,
