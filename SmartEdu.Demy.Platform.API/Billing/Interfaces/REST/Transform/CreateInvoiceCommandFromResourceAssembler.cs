84b0ca32ac0de98e40d568cdc01d95b8c093ef7c
Salim Ramirez <salimramirezm@gmail.com>
2025-07-05 22:02:51 -0500

    feat(billing): add CreateInvoiceCommandFromResourceAssembler in transform folder

---
+++ SmartEdu.Demy.Platform.API/Billing/Interfaces/REST/Transform/CreateInvoiceCommandFromResourceAssembler.cs
@@ -0,0 +1,17 @@
+using SmartEdu.Demy.Platform.API.Billing.Domain.Model.Commands;
+using SmartEdu.Demy.Platform.API.Billing.Interfaces.REST.Resources;
+
+namespace SmartEdu.Demy.Platform.API.Billing.Interfaces.REST.Transform;
+
+public static class CreateInvoiceCommandFromResourceAssembler
+{
+    public static CreateInvoiceCommand ToCommandFromResource(string dni, CreateInvoiceResource resource)
+    {
+        return new CreateInvoiceCommand(
+            dni,
+            resource.Amount,
+            resource.Currency,
+            resource.Date
+        );
+    }
+}
