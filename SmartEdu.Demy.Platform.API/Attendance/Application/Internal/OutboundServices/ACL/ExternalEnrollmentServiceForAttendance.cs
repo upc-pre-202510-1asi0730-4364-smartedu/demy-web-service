3044cdb3861bb12e9fe3259142b1791f938f2756
diegovilcasaboya <diegovilcatut@gmail.com>
2025-07-06 18:24:25 -0500

    feat: add ExternalEnrollmentServiceForAttendance ACL

---
+++ SmartEdu.Demy.Platform.API/Attendance/Application/Internal/OutboundServices/ACL/ExternalEnrollmentServiceForAttendance.cs
@@ -0,0 +1,11 @@
+﻿using SmartEdu.Demy.Platform.API.Enrollment.Interfaces.ACL;
+namespace SmartEdu.Demy.Platform.API.Attendance.Application.Internal.OutboundServices.ACL;
+
+public class ExternalEnrollmentServiceForAttendance(IEnrollmentsContextFacade enrollmentsContextFacade)
+{
+    public async Task<string> FetchStudentNameByDni(string dni)
+    {
+        var studentName = await enrollmentsContextFacade.FetchStudentFullNameByDni(dni);
+        return string.IsNullOrWhiteSpace(studentName) ? "" : studentName;
+    }
+}
