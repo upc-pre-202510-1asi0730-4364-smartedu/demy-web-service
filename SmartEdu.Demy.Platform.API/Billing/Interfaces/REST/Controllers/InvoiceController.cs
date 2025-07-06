3fc5510b02aa1c37667c896c2cbe5c69409a4bf8
Salim Ramirez <salimramirezm@gmail.com>
2025-07-05 23:10:13 -0500

    feat(billing): update InvoiceController.cs

--- SmartEdu.Demy.Platform.API/Billing/Interfaces/REST/Controllers/InvoiceController.cs
+++ SmartEdu.Demy.Platform.API/Billing/Interfaces/REST/Controllers/InvoiceController.cs
@@ -4,38 +4,50 @@ using SmartEdu.Demy.Platform.API.Billing.Domain.Model.Queries;
 using SmartEdu.Demy.Platform.API.Billing.Domain.Services;
 using SmartEdu.Demy.Platform.API.Billing.Interfaces.REST.Resources;
 using SmartEdu.Demy.Platform.API.Billing.Interfaces.REST.Transform;
+using SmartEdu.Demy.Platform.API.Shared.Domain.Model.ValueObjects;
 using Swashbuckle.AspNetCore.Annotations;
 
 namespace SmartEdu.Demy.Platform.API.Billing.Interfaces.REST.Controllers;
 
 [ApiController]
-[Route("api/v1/[controller]")]
+[Route("api/v1/students/{dni}/[controller]")]
 [Produces(MediaTypeNames.Application.Json)]
 [SwaggerTag("Available Invoice Endpoints.")]
 public class InvoiceController(
+        IInvoiceCommandService invoiceCommandService,
         IInvoiceQueryService invoiceQueryService
     ) : ControllerBase
 {
-    [HttpGet("{studentId:long}")]
-    [SwaggerOperation(
-        Summary = "Get All Student ID",
-        Description = "Get all invoices by student ID.",
-        OperationId = "GetAllInvoicesByStudentId"
-    )]
+    [HttpGet]
+    [SwaggerOperation]
     [SwaggerResponse(
         StatusCodes.Status200OK,
         "The invoices were found",
         typeof(IEnumerable<InvoiceResource>)
     )]
-    public async Task<IActionResult> GetAllInvoicesByStudentId(long studentId)
+    public async Task<IActionResult> GetAllInvoicesByDni([FromRoute] string dni)
     {
-        var getAllInvoicesByStudentIdQuery = new GetAllInvoicesByStudentIdQuery(studentId);
-        var invoices = await invoiceQueryService.Handle(getAllInvoicesByStudentIdQuery);
-        
+        var getAllInvoicesByDniQuery = new GetAllInvoicesByDniQuery(new Dni(dni));
+        var invoices = await invoiceQueryService.Handle(getAllInvoicesByDniQuery);
         var invoiceResources = invoices
             .Select(InvoiceResourceFromEntityAssembler.ToResourceFromEntity)
             .ToList();
-        
         return Ok(invoiceResources);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateInvoice([FromRoute] string dni, [FromBody] CreateInvoiceResource resource)
+    {
+        var createInvoiceCommand = CreateInvoiceCommandFromResourceAssembler.ToCommandFromResource(dni, resource);
+        var invoiceId = await invoiceCommandService.Handle(createInvoiceCommand);
+        var getInvoiceByIdQuery = new GetInvoiceByIdQuery(invoiceId);
+        var invoice = await invoiceQueryService.Handle(getInvoiceByIdQuery);
+        if (invoice is null) return BadRequest("Invoice could not be created.");
+        var invoiceResource = InvoiceResourceFromEntityAssembler.ToResourceFromEntity(invoice);
+        return CreatedAtAction(
+            nameof(GetAllInvoicesByDni),
+            new { dni = dni },
+            invoiceResource
+        );
     }
 }
