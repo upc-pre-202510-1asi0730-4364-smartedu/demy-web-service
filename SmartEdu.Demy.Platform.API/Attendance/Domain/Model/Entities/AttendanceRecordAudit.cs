ad987df1c1bef61ac65a01ec92a89e77675c7453
diegovilcasaboya <diegovilcatut@gmail.com>
2025-07-06 01:35:27 -0500

    feat: add AttendanceRecord audit

---
+++ SmartEdu.Demy.Platform.API/Attendance/Domain/Model/Entities/AttendanceRecordAudit.cs
@@ -0,0 +1,8 @@
+﻿namespace SmartEdu.Demy.Platform.API.Attendance.Domain.Model.Entities;
+using System.ComponentModel.DataAnnotations.Schema;
+using EntityFrameworkCore.CreatedUpdatedDate.Contracts;
+public partial class AttendanceRecord:IEntityWithCreatedUpdatedDate
+{
+    [Column("CreatedAt")]public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")]public DateTimeOffset? UpdatedDate { get; set; }
+}
