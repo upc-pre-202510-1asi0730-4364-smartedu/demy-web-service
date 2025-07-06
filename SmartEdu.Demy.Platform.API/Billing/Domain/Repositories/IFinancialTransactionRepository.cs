feaf1d9b60e33b79ae623fda697386ad73832e70
Salim Ramirez <salimramirezm@gmail.com>
2025-07-06 16:17:30 -0500

    feat(billing): update FinancialTransactionRepository interface

--- SmartEdu.Demy.Platform.API/Billing/Domain/Repositories/IFinancialTransactionRepository.cs
+++ SmartEdu.Demy.Platform.API/Billing/Domain/Repositories/IFinancialTransactionRepository.cs
@@ -5,5 +5,7 @@ namespace SmartEdu.Demy.Platform.API.Billing.Domain.Repositories;
 
 public interface IFinancialTransactionRepository : IBaseRepository<FinancialTransaction>
 {
+    Task<IEnumerable<FinancialTransaction>> ListWithPaymentAsync();
 
+    Task<FinancialTransaction?> FindByIdWithPaymentAsync(int id);
 }
