f9e519c8800aa79334c214071e14a7ec8b34609d
diegovilcasaboya <diegovilcatut@gmail.com>
2025-07-06 18:23:00 -0500

    feat: update ClassSessionRepository

--- SmartEdu.Demy.Platform.API/Attendance/Infrastructure/Repositories/ClassSessionRepository.cs
+++ SmartEdu.Demy.Platform.API/Attendance/Infrastructure/Repositories/ClassSessionRepository.cs
@@ -3,6 +3,7 @@ using SmartEdu.Demy.Platform.API.Attendance.Domain.Repositories;
 using SmartEdu.Demy.Platform.API.Shared.Infrastructure.Persistence.EFC.Configuration;
 using SmartEdu.Demy.Platform.API.Shared.Infrastructure.Persistence.EFC.Repositories;
 using Microsoft.EntityFrameworkCore;
+using SmartEdu.Demy.Platform.API.Attendance.Domain.Model.Entities;
 using SmartEdu.Demy.Platform.API.Shared.Domain.Repositories;
 
 namespace SmartEdu.Demy.Platform.API.Attendance.Infrastructure.Repositories;
@@ -15,4 +16,23 @@ public class ClassSessionRepository(AppDbContext context) : BaseRepository<Class
     {
       return await Context.Set<ClassSession>().FirstOrDefaultAsync(x => x.CourseId == courseId && x.Date == date);  
     }
+    
+    public async Task<List<AttendanceRecord>> FindAttendanceRecordsByDniCourseAndDateRangeAsync(
+        long courseId, string dni, DateOnly startDate, DateOnly endDate)
+    {
+        return await Context.Set<ClassSession>()
+            .Where(cs => cs.CourseId == courseId && cs.Date >= startDate && cs.Date <= endDate)
+            .SelectMany(cs => cs.Attendance
+                .Where(a => a.Dni == dni))
+            .ToListAsync();
+    }
+    public async Task<List<ClassSession>> FindSessionsByCourseAndDateRangeAsync(
+        long courseId, DateOnly startDate, DateOnly endDate)
+    {
+        return await Context.Set<ClassSession>()
+            .Where(cs => cs.CourseId == courseId && cs.Date >= startDate && cs.Date <= endDate)
+            .Include(cs => cs.Attendance) // asegúrate de incluir la relación
+            .ToListAsync();
+    }
+
 }
