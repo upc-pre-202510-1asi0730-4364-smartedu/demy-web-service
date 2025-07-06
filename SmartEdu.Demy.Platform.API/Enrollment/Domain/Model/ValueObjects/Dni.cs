82e42b49800fc8c37aa843604e8cc91626c99b6a
Kyrubi <u20221c486@upc.edu.pe>
2025-07-06 11:01:55 -0500

    docs(enrollment): add documentation for DNI VO

--- SmartEdu.Demy.Platform.API/Enrollment/Domain/Model/ValueObjects/Dni.cs
+++ SmartEdu.Demy.Platform.API/Enrollment/Domain/Model/ValueObjects/Dni.cs
@@ -1,7 +1,7 @@
 ﻿namespace SmartEdu.Demy.Platform.API.Enrollment.Domain.Model.ValueObjects
 {
     /// <summary>
-    /// Value object para un DNI
+    /// Value object DNI
     /// </summary>
     public record Dni
     {
