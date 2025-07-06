24da919173a1f35e3ae834ad59db01e62f38eb93
diegovilcasaboya <diegovilcatut@gmail.com>
2025-07-06 18:24:58 -0500

    feat: add ClassSessionReportFromEntityAssembler

---
+++ SmartEdu.Demy.Platform.API/Attendance/Interfaces/REST/Transform/ClassSessionReportFromEntityAssembler.cs
@@ -0,0 +1,22 @@
+﻿using SmartEdu.Demy.Platform.API.Attendance.Domain.Model.Aggregates;
+using SmartEdu.Demy.Platform.API.Attendance.Interfaces.REST.Resources;
+
+namespace SmartEdu.Demy.Platform.API.Attendance.Interfaces.REST.Transform;
+
+public static class ClassSessionReportFromEntityAssembler
+{
+    public static ClassSessionReportResource ToResourceFromEntities(long courseId, string dni, List<ClassSession> sessions)
+    {
+        var attendance = sessions
+            .SelectMany(session =>
+                session.Attendance
+                    .Where(record => record.Dni == dni)
+                    .Select(record =>
+                        AttendanceReportFromEntityAssembler.ToResourceFromEntity(record, session.Date)
+                    )
+            )
+            .ToList();
+
+        return new ClassSessionReportResource(courseId, attendance);
+    }
+}
