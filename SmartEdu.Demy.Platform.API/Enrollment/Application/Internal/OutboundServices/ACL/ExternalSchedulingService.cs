a2c553199569ee8beb31bd78198475b5fc44c9aa
Kyrubi <u20221c486@upc.edu.pe>
2025-07-05 22:32:29 -0500

    feat(enrollment): add ExternalSchedulingService ACL adapter

---
+++ SmartEdu.Demy.Platform.API/Enrollment/Application/Internal/OutboundServices/ACL/ExternalSchedulingService.cs
@@ -0,0 +1,33 @@
+﻿using SmartEdu.Demy.Platform.API.Scheduling.Interfaces.ACL;
+
+namespace SmartEdu.Demy.Platform.API.Enrollment.Application.Internal.OutboundServices.ACL
+{
+    /// <summary>
+    /// Service that retrieves data from the Scheduling module.
+    /// </summary>
+    /// <remarks>Paul Sulca</remarks>
+    public class ExternalSchedulingService
+    {
+        private readonly ISchedulingsContextFacade schedulingsContextFacade;
+
+        /// <summary>
+        /// Initializes a new instance of the ExternalSchedulingService class.
+        /// </summary>
+        /// <param name="schedulingsContextFacade">Facade to access Scheduling operations</param>
+        public ExternalSchedulingService(ISchedulingsContextFacade schedulingsContextFacade)
+        {
+            this.schedulingsContextFacade = schedulingsContextFacade;
+        }
+
+        /// <summary>
+        /// Gets the ID of a weekly schedule by its name.
+        /// </summary>
+        /// <param name="name">The name of the weekly schedule</param>
+        /// <returns>The ID of the weekly schedule, or 0 if not found</returns>
+        public async Task<int> FetchWeeklyScheduleIdByName(string name)
+        {
+            var weeklyScheduleId = await schedulingsContextFacade.FetchWeeklyScheduleIdByName(name);
+            return weeklyScheduleId == 0 ? 0 : weeklyScheduleId;
+        }
+    }
+}
