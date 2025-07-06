0f4da89a01af1d216ecf17ec35c3e563d5f3709d
diegovilcasaboya <diegovilcatut@gmail.com>
2025-07-06 18:22:06 -0500

    feat: add AttendanceReportFromEntityAssembler

---
+++ SmartEdu.Demy.Platform.API/Attendance/Interfaces/REST/Transform/AttendanceReportFromEntityAssembler.cs
@@ -0,0 +1,18 @@
+﻿using SmartEdu.Demy.Platform.API.Attendance.Domain.Model.Entities;
+using SmartEdu.Demy.Platform.API.Attendance.Interfaces.REST.Resources;
+
+namespace SmartEdu.Demy.Platform.API.Attendance.Interfaces.REST.Transform;
+
+
+public static class AttendanceReportFromEntityAssembler
+{
+    public static AttendanceReportResource ToResourceFromEntity(AttendanceRecord entity, DateOnly date)
+    {
+        return new AttendanceReportResource(
+            entity.Dni,
+            entity.StudentName ?? string.Empty,
+            entity.Status.ToString(),
+            date
+        );
+    }
+}
